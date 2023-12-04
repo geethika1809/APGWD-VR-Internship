b3fcfe4e0cb0df11585e6b589d393416e7054b5b
Viswanath Bodapati <81230933+Viswanath-621@users.noreply.github.com>
2023-12-04 16:45:15 +0530

    updated on 4th dec - V

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import PublicAcc from './components/publicAcc'
+import Navbar from './components/Navbar'
+import Herosec from './components/Herosec'
+import Footer from './components/Footer'
+import Login from './pages/Login'
+
+function App() {
+    return (
+      <BrowserRouter>
+    <div>
+    <Navbar/>
+    <Herosec/>
+    {/* <PublicAcc/> */}
+    {/* <Footer/> */}
+    </div>
+
+    <Routes>
+        <Route path='/Login' element = {<Login/>}></Route>
+    </Routes>
+        
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Login from '../pages/Login';
+
+
+function Navbar() {
+    
+
+        return (
+    <div className= "nav">
+        <div class="container-fluid">
+
+            <div className='Logos'> 
+                <img src="1-logo.png" alt="" />
+                <img src="2-logo.png" alt="" />
+                <img src="3-logo.png" alt="" />
+            </div>
+
+            <div class="page-title">
+                <p class="lead">
+                    Government of Andhra Pradesh
+                </p>
+            
+                <h2>Ground Water & Water Audit Department</h2>
+                
+                <p class="lead">
+                Water Resource Department
+                </p>
+            </div>
+
+            <div className='Logos-f'> 
+                <img src="4-logo.png" alt="" />
+            </div>
+
+            <div className='logind'>
+                <div>
+                    <a href="/Login" >Login Here</a>
+                </div>
+            </div>
+        </div>
+       
+       <div className='links-da'>
+            <ul>
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#news">NEWS</a></li>
+                <li><a href="#contact">AWARDS</a></li>
+                <li><a href="#about">APWRIMS</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+                <li><a href="#about">ABOUT US</a></li>
+            </ul>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
